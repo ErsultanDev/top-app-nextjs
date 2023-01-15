00b1b642c976f9d5c50b2f8ebd4fc0c98f6a0860
Ersultan <ersultansansyzbaj012@gmail.com>
2023-01-15 11:46:54 +0600

    first commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,35 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from "next/document";
+import Document from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  static async getInititalProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const intitalProps = await Document.getInitialProps(ctx);
+    return { ...intitalProps };
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang="ru">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+// export default function Document() {
+//   return (
+//     <Html lang="en">
+//       <Head />
+//       <body>
+//         <Main />
+//         <NextScript />
+//       </body>
+//     </Html>
+//   );
+// }
+
+export default MyDocument;
